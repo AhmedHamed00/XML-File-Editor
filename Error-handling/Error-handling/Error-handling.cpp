4b90c33dd15360253440780c4042683f0db39abc
OmarT-Y <omartamerz9@gmail.com>
2023-12-28 05:50:55 +0200

    Revert "dk man"
    This reverts commit d7c67d9a7f2bd4f9187edfdf7c48071e131d52be.

--- Error-handling/Error-handling/Error-handling.cpp
+++ Error-handling/Error-handling/Error-handling.cpp
@@ -4,17 +4,14 @@
 using namespace std;
 int main()
 {
-    do
+    uint8_t x;
+    if(find_errors("C:/Users/George/Downloads/sample.xml", x, 1))cout<<"Formatting fault"<<endl;
+    cout << "Number of errors : " << error_list.size() << endl;
+    for (auto a : error_list)
     {
-        uint8_t x;
-        if (find_errors("D:/ASU/7/DS/Project notes/sample.xml", x, 1))cout << "Formatting fault" << endl;
-        cout << "Number of errors : " << error_list.size() << endl;
-        for (auto a : error_list)
-        {
-            cout << a << endl;
-        }
-        solve_errors();
-    } while (error_list.size());
+        cout << a << endl;
+    }
+    solve_errors();
     return 0;
 }
 
